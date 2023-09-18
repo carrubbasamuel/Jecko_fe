7eaa7e42a4a91b43867927e11787c81dbb540916
Samuel Carrubba <97916309+carrubbasamuel@users.noreply.github.com>
2023-09-18 19:42:21 +0200

    fix env

--- src/redux/socketReducer.js
+++ src/redux/socketReducer.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 
 
 const initialState = {
-    socket: io.connect(process.env.REACT_APP_BACK_URL + "/chat")
+    socket: io.connect(process.env.REACT_APP_SOCKET_URL + "/chat")
 };
 
 const socketSlice = createSlice({
